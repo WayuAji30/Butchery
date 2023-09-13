34ee9118daff7bb1374441430f35556f72dcbe92
WayuAji30 <wahyuma123@gmail.com>
2023-09-13 19:31:25 +0700

    add js function while button + click change totalValue

--- src/js/product.js
+++ src/js/product.js
@@ -1,19 +1,33 @@
-// Menaikan jumlah Product
-const plus = document.querySelector("#plus"),
-  mines = document.querySelector("#mines");
-value = document.querySelector("#value-product");
+document.addEventListener("DOMContentLoaded", function () {
+  // Ambil elemen-elemen yang diperlukan
+  const plusButton = document.getElementById("plus");
+  const minusButton = document.getElementById("mines");
+  const valueProduct = document.getElementById("value-product");
+  const totalValue = document.getElementById("total-value");
 
-let a = 1;
+  // Tambahkan event listener ke tombol plus
+  plusButton.addEventListener("click", function () {
+    valueProduct.textContent = parseInt(valueProduct.textContent) + 1;
+    updateTotal();
+  });
 
-plus.addEventListener("click", () => {
-  a++;
-  value.innerText = a;
-  console.log(a);
-});
+  // Tambahkan event listener ke tombol minus
+  minusButton.addEventListener("click", function () {
+    if (parseInt(valueProduct.textContent) > 1) {
+      valueProduct.textContent = parseInt(valueProduct.textContent) - 1;
+      updateTotal();
+    }
+  });
 
-mines.addEventListener("click", () => {
-  if (a > 1) {
-    a--;
-    value.innerText = a;
+  // Fungsi untuk memperbarui nilai total
+  function updateTotal() {
+    const value = parseInt(valueProduct.textContent);
+    const price = 99000; // Harga per produk (misalnya 99.000)
+
+    // Hitung total
+    const total = value * price;
+
+    // Perbarui tampilan nilai total
+    totalValue.textContent = total.toLocaleString("id-ID"); // Format sebagai mata uang Indonesia
   }
 });
