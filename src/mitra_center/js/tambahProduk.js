658b492fb3bf05d65222f4d002d72aa7e126660f
WayuAji30 <wahyuma123@gmail.com>
2023-09-26 14:48:42 +0700

    add searchProduct page

--- src/mitra_center/js/tambahProduk.js
+++ src/mitra_center/js/tambahProduk.js
@@ -4,7 +4,7 @@ document
     // Menghilangkan elemen flex dan items-center di bagian form
     var formElement = document
       .getElementById("varian-plus")
-      .querySelector("form");
+      .querySelector("div");
     formElement.style.display = "block";
     formElement.style.alignItems = "initial";
 
@@ -19,3 +19,15 @@ document
       element.removeAttribute("hidden");
     });
   });
+
+// FUCTION UNTUK MENGARAHKAN BUTTON SIMPAN KE LOGIN.HTML TANPA PERLU REQUIRED DULU
+document.addEventListener("DOMContentLoaded", function () {
+  const cancelButton = document.getElementById("cancelButton");
+
+  cancelButton.addEventListener("click", function (event) {
+    event.preventDefault(); // Mencegah formulir dikirim
+
+    // Mengarahkan ke ../index.html
+    window.location.href = "daftarProduk.html";
+  });
+});
