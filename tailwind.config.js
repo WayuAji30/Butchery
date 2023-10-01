1f85a3ba03bcf27acb47103686b5cb4e5a92e218
R. Bramaditya <rbramaditya@gmail.com>
2023-10-01 15:46:20 +0700

    mobile redirect to mobile.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,4 +7,14 @@ module.exports = {
     },
   },
   plugins: [],
+  screens: {
+    sm: { min: "375px", max: "767px" },
+    // => @media (min-width: 640px and max-width: 767px) { ... }
+
+    md: { min: "768px", max: "1023px" },
+    // => @media (min-width: 768px and max-width: 1023px) { ... }
+
+    lg: { min: "1024px" },
+    // => @media (min-width: 1024px) { ... }
+},
 };
